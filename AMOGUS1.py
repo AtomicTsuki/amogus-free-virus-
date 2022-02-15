cce44eea2d20b0e1f36954d1296278a955b7ae9d
ExplodingAstro <75099719+ExplodingAstro@users.noreply.github.com>
2022-02-15 00:07:10 +0000

    Add files via upload

---
+++ AMOGUS1.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import webbrowser
+from tkinter import messagebox
+import os
+
+window = Tk()
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
+window.withdraw()
+
+if messagebox.askyesno('Amogus', 'Are you the impostor?!', icon='error') == True:
+    for i in range(100):
+        print("PRETTY SUS NGL")
+    ##os.system('Walt-Sussy-.mp4')
+    ##os.system('AMOGUS.mp4')
+    for i in range(10):
+        webbrowser.open('https://cdn.discordapp.com/attachments/534420663344037890/869402534803013682/video0_39.mp4')
+    for i in range(10):
+        os.system(command)
+    for i in range(10):
+        messagebox.showerror("YOU HAVE BEEN HACKED BY THE AMOGUSES")
+    webbrowser.open('https://www.google.com/search?q=when+the+imposter+is+sus&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiavvm2lYLyAhXzTxUIHQZyBt8Q_AUoAXoECAEQAw&biw=1920&bih=937#imgrc=YxOVZ8wouxyAQM')
+    for i in range(5):
+        webbrowser.open('https://www.youtube.com/watch?v=0bZ0hkiIKt0')
+        webbrowser.open('https://www.youtube.com/watch?v=lrrAYV7jDLE')
+        webbrowser.open('https://www.youtube.com/watch?v=lrrAYV7jDLE')
+    for i in range(100):
+        print("YOU HAVE BEEN HACKED BY BASED AMOGUS LOL")
+
+    if messagebox.askyesno('Amogus War', 'Do you want THIS TO CARRY ON???', icon='error') == True:
+        messagebox.showerror('WELL TOO BAD')
+        for i in range(10):
+            print("TIME TO HACK")
+            os.system(command)
+
+
+window.deiconify()
+window.destroy()
+window.quit()
